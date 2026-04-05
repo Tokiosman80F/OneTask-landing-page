549e56f996b0e4db8e96cef1ec0192714cd0adef
unknown <tokiosman96@gmail.com>
2026-04-05 22:35:57 +0600

    feat: add CTA component structure with responsive layout,image pending

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,3 +1,12 @@
 export const CallToAction = () => {
-  return null;
+  return <div className=" sm:py-24 | bg-black text-white py-[72px] ">
+    <div className="container max-w-xl text-center">
+      <h2 className="font-bold tracking-tighter text-5xl">Get instant access</h2>
+      <p className="text-xl text-white/70 mt-5">Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
+      <form className="sm:flex-row sm:max-w-sm mx-auto | mt-10 flex flex-col gap-2.5">
+        <input type="email" placeholder="your@email.com" className=" sm:flex-1 | h-12 px-5  font-medium bg-white/20 rounded-lg placeholder:text-[#9CA3AF]" />
+        <button className=" px-5 h-12 font-medium bg-white rounded-lg text-black">Get access</button>
+      </form>
+    </div>
+  </div>;
 };
