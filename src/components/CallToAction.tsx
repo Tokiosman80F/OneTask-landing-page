6feb513f288656217af15de7ead8356fe1924eeb
unknown <tokiosman96@gmail.com>
2026-04-05 22:50:41 +0600

    feat: add image to CTA component

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,6 +1,14 @@
+import helixStarImage from '../assets/images/helix2.png'
+import emojiStarImage from '../assets/images/emojistar.png'
+import Image from 'next/image';
+
 export const CallToAction = () => {
   return <div className=" sm:py-24 | bg-black text-white py-[72px] ">
-    <div className="container max-w-xl text-center">
+    <div className="container relative max-w-xl text-center">
+
+      <Image src={helixStarImage} alt="Helix star image" className='absolute  left-[calc(100%+24px)]'/>
+      <Image src={emojiStarImage} alt="Helix star image" className='absolute right-[calc(100%+24px)] -top-[124px]'/>
+
       <h2 className="font-bold tracking-tighter text-5xl">Get instant access</h2>
       <p className="text-xl text-white/70 mt-5">Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
       <form className="sm:flex-row sm:max-w-sm mx-auto | mt-10 flex flex-col gap-2.5">
