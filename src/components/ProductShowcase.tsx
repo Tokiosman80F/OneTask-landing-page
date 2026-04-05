c84c2e3d30aca9e06b05f3764f81b868ad4e1569
unknown <tokiosman96@gmail.com>
2026-04-05 21:18:30 +0600

    feat:add productShowcase responsive UI component

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
 
 export const ProductShowcase = () => {
-  return null;
+  return <div className="sm:py-24 | bg-black text-white py-[72px] bg-gradient-to-b from-black to-[#5D2CA8]">
+    <div className="container">
+      <h2 className="sm:text-6xl | text-5xl font-bold  tracking-tighter text-center">Intuitive interface</h2>
+      <div className="max-w-xl mx-auto">
+        <p className="text-xl text-white/70 mt-5 text-center">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes, one task at a time.</p>
+      </div>
+      <div className="mt-14">
+        <Image src={appScreen} alt="product platform"/>
+      </div>
+    </div>
+  </div>;
 };
