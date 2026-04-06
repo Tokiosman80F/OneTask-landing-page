efbf396d96233922f4746a5f4a8c938f9b855296
Tokiosman80F <tokiosman96@gmail.com>
2026-04-06 19:36:38 +0600

    feat:add animation in productShowcase component

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,15 +1,52 @@
+"use client";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 import appScreen from "../assets/images/app-screen.png";
 
 export const ProductShowcase = () => {
-  return <div className="sm:py-24 | bg-black text-white py-[72px] bg-gradient-to-b from-black to-[#5D2CA8]">
-    <div className="container ">
-      <h2 className="sm:text-6xl | text-5xl font-bold  tracking-tighter text-center">Intuitive interface</h2>
-      <div className="max-w-xl mx-auto">
-        <p className="text-xl text-white/70 mt-5 text-center">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes, one task at a time.</p>
+  const appImage = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImage,
+    offset: ["start end", "end end"],
+  });
+
+  useEffect(() => {
+    scrollYProgress.on("change", (latestValue) =>
+      console.log("latest value", latestValue),
+    );
+  }, []);
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+  return (
+    <div className="sm:py-24 | bg-black text-white py-[72px] bg-gradient-to-b from-black to-[#5D2CA8]">
+      <div className="container ">
+        <h2 className="sm:text-6xl | text-5xl font-bold  tracking-tighter text-center">
+          Intuitive interface
+        </h2>
+        <div className="max-w-xl mx-auto">
+          <p className="text-xl text-white/70 mt-5 text-center">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes,
+            one task at a time.
+          </p>
+        </div>
+        <motion.div
+          style={{
+            opacity: opacity,
+            rotateX: rotateX,
+            transformPerspective: "800px",
+          }}
+        >
+          <Image
+            src={appScreen}
+            alt="product platform"
+            className="mt-14 mx-auto"
+            ref={appImage}
+          />
+        </motion.div>
       </div>
-        <Image src={appScreen} alt="product platform" className="mt-14 mx-auto"/>
-      
     </div>
-  </div>;
+  );
 };
